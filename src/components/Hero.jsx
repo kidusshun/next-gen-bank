accd30780fb519248bc16421ae651f6f76432de0
kidusshun <kidusshun@gmail.com>
2023-03-04 14:37:46 +0300

    navbar done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles';
+import {discount, robot } from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-6`}>
+
+
+      </div>
+    </section>
   )
 }
 
