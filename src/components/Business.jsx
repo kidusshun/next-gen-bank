325caa9d7f5c8308ee4fcae905419e286984b5ad
kidusshun <kidusshun@gmail.com>
2023-03-05 08:41:37 +0300

    business section done

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -10,7 +10,7 @@ const FeatureCard = ({icon, title, content, index}) => (
     </div>
     <div className='flex-1 flex flex-col ml-3'>
       <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>{title}</h4>
-      <p className='font-poppins font-normal text-dimWhite text-[18px] leading-[23px] mb-1'>{content}</p>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>{content}</p>
     </div>
   </div>
 )
