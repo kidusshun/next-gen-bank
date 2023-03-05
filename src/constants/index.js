234752b18551c4caa189ba6ac0d05a7df642d5a5
kidusshun <kidusshun@gmail.com>
2023-03-05 18:29:07 +0300

    footer done

--- src/constants/index.js
+++ src/constants/index.js
@@ -156,22 +156,22 @@ export const footerLinks = [
 
 export const socialMedia = [
   {
-    id: "social-media-1",
+    id: "instagram",
     icon: instagram,
     link: "https://www.instagram.com/",
   },
   {
-    id: "social-media-2",
+    id: "facebook",
     icon: facebook,
     link: "https://www.facebook.com/",
   },
   {
-    id: "social-media-3",
+    id: "twitter",
     icon: twitter,
     link: "https://www.twitter.com/",
   },
   {
-    id: "social-media-4",
+    id: "linkedin",
     icon: linkedin,
     link: "https://www.linkedin.com/",
   },
