471c63cc94472e33e058dbdfabf6e542c699da46
unknown <kidusshun@gmail.com>
2023-03-16 13:52:58 +0300

    link adjusted

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 
 const CardDeal = () => {
   return (
-    <section id='features' className={layout.section}>
+    <section id='product' className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>Find a better card deal<br className='sm:block hidden'/>in few easy steps.</h2>
         <p className={`${styles.paragraph} max-w-[540px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
